ea983a98a05e946dea21cc54c6c9183e7821d3ce
alee <zq@alee2002.com>
2012-01-09 14:42:00 +0800

    create extension

---
+++ class/Qzonefeedpull.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\qzonefeedpull ;
+
+use org\opencomb\platform\ext\Extension ;
+
+class Qzonefeedpull extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		// todo ...
+	}
+}
